0aa5da486a44ec1f60cb0e72d88c974dfa5b3094
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-05-06 10:27:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,44 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const onInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+  const onAdd = () => {
+    setTasks((prev) => [...prev, input]);
+    setInput("");
+  };
+
+  const onDelet = (ind) => {
+    const newTasks = [...tasks];
+    newTasks.splice(ind, 1);
+    setTasks(newTasks);
+  };
+  return (
+    <div>
+      <p>To-Do List</p>
+
+      <div>
+        <input value={input} onChange={onInput} />
+        <button onClick={onAdd}>Add Todo</button>
+      </div>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <span>{item}</span>
+            <button onClick={() => onDelet(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
